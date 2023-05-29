97c596367a6324c34c2ec8b917c0c004b5de2f90
carlosnacamura <127620688+carlosnacamura@users.noreply.github.com>
2023-05-29 03:23:08 -0300

    Quase finalizado

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
-const swiper = new Swiper('.swiper', {
-    direction: 'horizontal',
-    loop: true,
-    effect: 'coverflow',
-    centerSlide: true,
-    grapCursor: true,
-    fade: true,
-    pagination: {
-      el: '.swiper-pagination',
-      dynamicBullets: true,
-      clickable: true,
-    },
-    coverflowEffect: {
-      rotate: 0,
-    },
-    keyboard: {
-      enabled: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-   
+function programção(elemento){
+  const textArrray = elemento.innerHTML.split('')
+  elemento.innerHTML = ''
+  textArrray.forEach((letra,i) => {
+    setTimeout(()=> elemento.innerHTML += letra,90*i)
+    
   });
-  
+}
+programção(document.querySelector('.titulo'))
+programção(document.querySelector('.box_main p'))
+const ulSquares = document.querySelector("ul.squares")
+
+
+for ( let i = 0; i < 31; i++) {
+    
+    const random= (min,max) => Math.random() * (max-min)+min
+    const li = document.createElement("li")
+
+    const size = Math.floor(random(10,130))
+
+    const position = random(1,99);
+    
+    const delay = random(5,0.1)
+
+    li.style.width =`${size}px`
+    li.style.height =`${size}px`
+    li.style.bottom =`-${size}px`
+
+    li.style.left =`${position}%`
+    li.style.animationDelay =`${delay}s`
+
+    ulSquares.appendChild(li)
+}
